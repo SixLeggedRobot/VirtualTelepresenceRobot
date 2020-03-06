b94fe278e607c95e09578abf9ce55600801e788f
SixLeggedRobot <61043714+SixLeggedRobot@users.noreply.github.com>
2020-03-06 12:49:57 +0530

    Update obstacle_detection.py

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -144,6 +144,7 @@ while True:
         pulse_end = time.time()
 
         pulse_duration = pulse_end - pulse_start
+        distance=pulse_duration*17150
         distance=round(distance,2)
         avgDistance=avgDistance+distance
 
